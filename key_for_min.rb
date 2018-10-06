3067594614b62e2b604a62ab3f65604012aac734
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-06 21:30:48 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,8 +10,8 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
   min = 1000000000
   
-  if value< min
-    min = value
+  if value< mini
+    mini = value
     theKey = key
   end
 end
